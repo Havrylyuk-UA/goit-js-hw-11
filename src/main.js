e75f92865904518d9521c612f0ad33862e402d1d
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2023-11-25 15:03:06 +0200

    hw v1.0

--- src/main.js
+++ src/main.js
@@ -1,25 +1,55 @@
-import iziToast from "izitoast";
-import SimpleLightbox from "simplelightbox";
+import iziToast from 'izitoast';
+import SimpleLightbox from 'simplelightbox';
 
 const form = document.querySelector('.form'),
-      searchInp = document.querySelector('input[type="text"]'),
-      errorDiv = document.querySelector('.error-con');
+  searchInp = document.querySelector('input[type="text"]'),
+  errorDiv = document.querySelector('.error-con'),
+  gallery = document.querySelector('.gallery')
 
-      // key — твій унікальний ключ доступу до API.
-      // q — слово для пошуку. Те, що буде вводити користувач.
-      // image_type — тип зображення. Потрібні тільки фотографії, тому постав значення photo.
-      // orientation — орієнтація фотографії. Постав значення horizontal.
-      // safesearch — фільтр за віком. Постав значення true.
+  let searchWord = '';
 
-      const postToAdd = {
-        title: "CRUD",
-        body: "CRUD is awesome!",
-      };
+  searchInp.addEventListener('input', e => {
+    e.preventDefault();
 
-      const options = {
-        method: "POST",
-        body: JSON.stringify(postToAdd),
-        headers: {
-          "Content-Type": "application/json; charset=UTF-8",
-        },
-      };
+    searchWord = e.target.value;
+  })
+
+    const API_KEY = '40876862-5828b09b8a35d05d7759eed0a';
+
+
+
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      gallery.innerHTML = ''
+
+      const waitInfo = 'Loading images, please wait...'
+      gallery.insertAdjacentHTML("afterbegin", waitInfo)
+
+      fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${searchWord}&image_type=photo&orientation=horizontal&safesearch=true`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        gallery.textContent = '';
+        const dataList = data.hits.map(photo => {
+          return `<li><img class="api-img" src="${photo.webformatURL}" alt="${photo.tags}"></li>`;
+        }).join("");
+        gallery.insertAdjacentHTML("afterbegin", dataList);
+
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        iziToast.show({
+          icon: 'icon-false',
+          backgroundColor: '#FC5A5A',
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+          messageColor: '#FAFAFB',
+          messageSize: '16px',
+          position: 'topCenter',
+          close: false,
+        });
+      });
+  });
