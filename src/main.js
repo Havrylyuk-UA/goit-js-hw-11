7227b89417da8b2009847840cbab0d2726180e42
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2023-11-25 13:22:22 +0200

    1

--- src/main.js
+++ src/main.js
@@ -1,2 +1,25 @@
 import iziToast from "izitoast";
 import SimpleLightbox from "simplelightbox";
+
+const form = document.querySelector('.form'),
+      searchInp = document.querySelector('input[type="text"]'),
+      errorDiv = document.querySelector('.error-con');
+
+      // key — твій унікальний ключ доступу до API.
+      // q — слово для пошуку. Те, що буде вводити користувач.
+      // image_type — тип зображення. Потрібні тільки фотографії, тому постав значення photo.
+      // orientation — орієнтація фотографії. Постав значення horizontal.
+      // safesearch — фільтр за віком. Постав значення true.
+
+      const postToAdd = {
+        title: "CRUD",
+        body: "CRUD is awesome!",
+      };
+
+      const options = {
+        method: "POST",
+        body: JSON.stringify(postToAdd),
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+        },
+      };
