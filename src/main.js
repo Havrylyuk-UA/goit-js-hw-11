43e0dbeccfe370bf482c9d2b7b4bdc8e1ae7695e
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2023-11-26 12:15:20 +0200

    final line hw v1.1

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import SimpleLightbox from 'simplelightbox';
 
 const form = document.querySelector('.form'),
   searchInp = document.querySelector('input[type="text"]'),
-  errorDiv = document.querySelector('.error-con'),
-  gallery = document.querySelector('.gallery');
+  gallery = document.querySelector('.gallery'),
+  loader = document.querySelector('.loader');
 
 let searchWord = '';
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 searchInp.addEventListener('input', e => {
   e.preventDefault();
@@ -14,14 +16,21 @@ searchInp.addEventListener('input', e => {
   searchWord = e.target.value;
 });
 
-const API_KEY = '40876862-5828b09b8a35d05d7759eed0a';
+const falseToast = {
+  icon: 'icon-false',
+  backgroundColor: '#FC5A5A',
+  message:
+    'Sorry, there are no images matching your search query. Please try again!',
+  messageColor: '#FAFAFB',
+  messageSize: '16px',
+  position: 'topRight',
+  close: false,
+};
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  gallery.innerHTML = '';
 
-  const waitInfo = 'Loading images, please wait...';
-  gallery.insertAdjacentHTML('afterbegin', waitInfo);
+  loader.style.display = 'inline-block';
 
   fetch(
     `https://pixabay.com/api/?key=${API_KEY}&q=${searchWord}&image_type=photo&orientation=horizontal&safesearch=true`
@@ -35,47 +44,32 @@ form.addEventListener('submit', e => {
     .then(data => {
       gallery.textContent = '';
       if (data.hits.length === 0) {
-        return iziToast.show({
-          icon: 'icon-false',
-          backgroundColor: '#FC5A5A',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          messageColor: '#FAFAFB',
-          messageSize: '16px',
-          position: 'topCenter',
-          close: false,
-        });
+        return iziToast.show(falseToast);
       }
-      const dataList = data.hits
-        .map(
-          ({
-            largeImageURL,
-            webformatURL,
-            tags,
-            likes,
-            views,
-            comments,
-            downloads,
-          }) => {
-            return `
-          <li class="gallery-item">
-          <a href="${largeImageURL}">
-          <img class="api-img" src="${webformatURL}" alt="${tags}">
+      const dataList = data.hits.map(photo => {
+        return `
+        <li class="gallery-item">
+          <a href="${photo.largeImageURL}">
+            <img class="api-img" src="${photo.webformatURL}" alt="${photo.tags}">
+            <div class="img-desc">
+              <span><b>Likes:</b> <br/>${photo.likes}</span>
+              <span><b>Views:</b> <br/>${photo.views}</span>
+              <span><b>Comments:</b> <br/>${photo.comments}</span>
+              <span><b>Downloads:</b> <br/>${photo.downloads}</span>
+            </div>
           </a>
-          <div class="img-desc">
-          <span><b>Likes:</b> <br/>${likes}</span>
-          <span><b>Views:</b> <br/>${views}</span>
-          <span><b>Comments:</b> <br/>${comments}</span>
-          <span><b>Downloads:</b> <br/>${downloads}</span>
-          </div>
-          </li>
-          `;
-          }
-        )
-        .join('');
-      gallery.insertAdjacentHTML('afterbegin', dataList);
+        </li>
+                  `;
+      });
+
+      gallery.insertAdjacentHTML('afterbegin', dataList.join(''));
+
+      loader.style.display = 'none';
+
       modalImg.refresh();
+
       searchWord = '';
+
       form.reset();
     })
     .catch(error => {
